d68b03800477d3b899a8bb78f1660c0c7167cc0c
Mauricio Rodrigues <mauricio12344663@gmail.com>
2026-07-17 14:45:06 +0100

    Fix Vercel Speed Insights import for React
    Changed Speed Insights import from the Next.js package to the React package since this is a React application, not a Next.js application.

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import Contactos from "./Contactos";
 import SobreNos from "./SobreNos.jsx";
 import Loader from "./Loader";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./css/style.css";
 
 function App() {
